9ae1afe245e615a1f5c3756ccd5ace1fd42d7366
praison-j <praison47745@gmail.com>
2026-03-17 13:40:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector("form").addEventListener("submit",function(e){
+
+e.preventDefault()
+
+alert("Message sent successfully!")
+
+})
+ScrollReveal().reveal('#home', { delay:200, origin:'bottom', distance:'50px' });
+ScrollReveal().reveal('#about', { delay:200, origin:'left', distance:'50px' });
+ScrollReveal().reveal('#skills', { delay:200, origin:'right', distance:'50px' });
+ScrollReveal().reveal('#projects', { delay:200, origin:'bottom', distance:'50px' });
+ScrollReveal().reveal('#contact', { delay:200, origin:'bottom', distance:'50px' });
